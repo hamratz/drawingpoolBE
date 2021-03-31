ab0f0437b1c9adbcb8fd47e49d1dc4a22f08a70e
Sandra Wazaz <sandrawazaz@gmail.com>
2021-03-31 08:54:46 -0400

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
 
   get "/drawings", to: "drawings#index"
+  post "/drawings", to: "toys#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
