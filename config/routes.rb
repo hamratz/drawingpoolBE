2cd9b89ef8fd3ea1a8acffab5b5c442c1f3fb1e8
Sandra Wazaz <sandrawazaz@gmail.com>
2021-03-31 16:50:20 -0400

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  resources :comments
+  
   # resources :drawings
-  resources :users
+
 
 
   get "/drawings", to: "drawings#index"
-  post "/drawings", to: "toys#create"
+  post "/drawings", to: "drawings#create"
+
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
+  post "/users", to: "users#create"
+
+  get "/comments", to: "comments#index"
+  post "/comments", to: "comments#create"
+  put "/comments/:id", to: "comments#update"
+  delete "/comments/:id", to: "comments#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
